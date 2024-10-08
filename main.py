9dab86a4f5190a45ec514902393778d363457927
рс <suleyma20n@mail.ru>
2024-10-08 21:08:51 +0300

    описана функция get_response

--- main.py
+++ main.py
@@ -11,7 +11,30 @@ def get_response(departure, arrival) -> requests.Response:
     # Подсказка: для запроса использовать POST запрос на https://rfinder.asalink.net/free/autoroute_rtx.php
     # с параметрами, указанными в options.txt, в которых вместо uuee и urml использовать входные параметры этой функции
     # https://pythonru.com/biblioteki/kratkoe-rukovodstvo-po-biblioteke-python-requests, "Передача параметров в URL"
-    pass
+    url = 'https://rfinder.asalink.net/free/autoroute_rtx.php'
+    try:
+        r = requests.post(url, data={'dbid': '2410',
+                                     'easet': 'Y',
+                                     'ic1': '',
+                                     'ic2': '',
+                                     'id1': departure,
+                                     'id2': arrival,
+                                     'k': '1233505483',
+                                     'lvl': 'B',
+                                     'maxalt': 'FL330',
+                                     'minalt': 'FL330',
+                                     'nats': 'R',
+                                     'rnav': 'Y',
+                                     'usesid': 'Y',
+                                     'usestar': 'Y'})
+        if r.status_code == 200:
+            print(r.text)
+            return r
+    except Exception as e:
+        print(e)
+
+
+print(get_response('uuee', 'urml'))
 
 
 def get_points(response: requests.Response) -> list[str]:
